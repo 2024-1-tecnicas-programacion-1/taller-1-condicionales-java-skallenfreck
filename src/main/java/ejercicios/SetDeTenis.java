62f146554c37cded805ae56498e38cefd3e0b0b3
Alexander Chacon <fresnedaalexander@gmail.com>
2024-03-04 22:55:08 -0500

    Desarrollo de los casos de condición
    Asi mismo, los test asignados para cada caso particular.

--- src/main/java/ejercicios/SetDeTenis.java
+++ src/main/java/ejercicios/SetDeTenis.java
@@ -7,19 +7,41 @@ import java.util.Scanner;
  * @author danielsanchez
  */
 public class SetDeTenis {
+
     public static String evaluar(int numVictoriasA, int numVictoriasB) {
-        // TODO: Coloca aquí el código del ejercicio 1: Set de tenis
+
+        if (numVictoriasA < 7 && numVictoriasB < 7 && Math.abs(numVictoriasA - numVictoriasB) < 2) {
+            return "Aún no termina";
+        } else if (numVictoriasA == 7 && numVictoriasB == 0) {
+            return "Ganó A";
+
+        } else if (numVictoriasA == 0 && numVictoriasB == 7) {
+            return "Ganó B";
+        } else if ((numVictoriasB <= numVictoriasA - 2) && (numVictoriasA > numVictoriasB) && (numVictoriasA <= 6)) {
+            return "Ganó A";
+        } else if ((numVictoriasA <= numVictoriasB - 2) && (numVictoriasB > numVictoriasA) && (numVictoriasB <= 6)) {
+            return "Ganó B";
+        } else if ((5 <= numVictoriasA) && (numVictoriasA <= 7) && (5 <= numVictoriasB) && (numVictoriasB <= 7)) {
+            if ((numVictoriasA - 1 > numVictoriasB)) {
+                return "Ganó A";
+            } else if (numVictoriasB - 1 > numVictoriasA) {
+                return "Ganó B";
+            }
+        } else if (((numVictoriasA >= 7) && (numVictoriasB < 7)) || ((numVictoriasB >= 7) && (numVictoriasA < 7))) {
+            return "Inválido";
+
+        }
         return "";
     }
-    
+
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-        
+
         System.out.print("Los juegos ganador por A:");
         int numVictoriasA = lector.nextInt();
         System.out.print("Los juegos ganador por B:");
         int numVictoriasB = lector.nextInt();
-        
+
         String respuesta = evaluar(numVictoriasA, numVictoriasB);
         System.out.println(respuesta);
     }
--- src/test/java/test/SetDeTenisTest.java
+++ src/test/java/test/SetDeTenisTest.java
@@ -13,12 +13,70 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author danielsanchez
  */
 public class SetDeTenisTest {
+
     @Test
     public void testAunNoTermina() {
         String valorEsperado = "Aún no termina";
         String valorActual = SetDeTenis.evaluar(4, 5);
         assertEquals(valorEsperado, valorActual);
     }
-    
+
+    @Test
+    public void testAunNoTermina2() {
+        String valorEsperado = "Aún no termina";
+        String valorActual = SetDeTenis.evaluar(5, 6);
+        assertEquals(valorEsperado, valorActual);
+    }
+
+    @Test
+    public void testGanoB() {
+        String valorEsperado = "Ganó B";
+        String valorActual = SetDeTenis.evaluar(5, 7);
+        assertEquals(valorEsperado, valorActual);
+    }
+
+    @Test
+    public void testGanoB2() {
+        String valorEsperado = "Ganó B";
+        String valorActual = SetDeTenis.evaluar(0, 6);
+        assertEquals(valorEsperado, valorActual);
+    }
+
+    @Test
+    public void testInvalido() {
+        String valorEsperado = "Inválido";
+        String valorActual = SetDeTenis.evaluar(3, 7);
+        assertEquals(valorEsperado, valorActual);
+
+    }
+
+    @Test
+    public void testInvalido2() {
+        String valorEsperado = "Inválido";
+        String valorActual = SetDeTenis.evaluar(8, 6);
+        assertEquals(valorEsperado, valorActual);
+    }
+
+    @Test
+    public void testInvalido3() {
+        String valorEsperado = "Inválido";
+        String valorActual = SetDeTenis.evaluar(7, 3);
+        assertEquals(valorEsperado, valorActual);
+    }
+
+    @Test
+    public void testGanoA() {
+        String valorEsperado = "Ganó A";
+        String valorActual = SetDeTenis.evaluar(6, 4);
+        assertEquals(valorEsperado, valorActual);
+    }
+
+    @Test
+    public void testGanoA2() {
+        String valorEsperado = "Ganó A";
+        String valorActual = SetDeTenis.evaluar(7, 0);
+        assertEquals(valorEsperado, valorActual);
+    }
+
     // TODO: Agrega tus otros casos de prueba aquí
 }
